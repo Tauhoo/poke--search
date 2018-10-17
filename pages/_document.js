a7f65fd8f0dc9f01245f3da97151124d1c38bb87
wachirawit wacharak <tauhoo_ice@hotmail.com>
2018-10-17 09:40:59 +0700

    set up project.

---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <style>{`
+            body {
+              margin: 0px;
+              padding: 0px;
+            }
+            * {
+              box-sizing: border-box;
+            }
+          `}</style>
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import styled from "styled-components";
+
+export default () => <h1>ICEICEICEICE</h1>;
